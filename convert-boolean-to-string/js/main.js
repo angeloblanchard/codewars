c10f0e515da2fcc58c6e41f0835a2d91e29202a8
Angelo Blanchard <angelo.blanchard@hotmail.com>
2022-05-15 18:26:15 -0700

    Function takes in a boolean and returns the value in its string form. This can be done in one line using a ternary operator.

---
+++ convert-boolean-to-string/js/main.js
@@ -0,0 +1,14 @@
+/* Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+PREP
+P: b: boolean value to be converted
+R: return the string version of the boolean
+E: true would be converted and returned as "true"
+P: truthy flag returns "true", falsy returns "false"
+*/
+
+function booleanToString(b){
+    return b? "true" : "false";
+}
